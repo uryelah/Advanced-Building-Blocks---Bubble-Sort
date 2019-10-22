77745b2ceb58f23753d89d2413b9d6e0781319d2
sarah <sarachamorro@gmail.com>
2019-10-22 14:42:15 -0300

    CHANGE VARIABLE IS_SORTED NAME

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
 def bubble_sort(arr)
-    is_sorted = false
-    counter = 1
-    while !is_sorted
-        is_sorted = true
-        arr.each_with_index { |n, i|
-            if i + counter < arr.length && n > arr[i + 1]
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                is_sorted = false
-            end
-        }  
-        counter += 1
-    end
-    arr
+  is_unsorted = true
+  counter = 1
+  while is_unsorted
+    is_unsorted = false
+    arr.each_with_index { |n, i|
+      if i + counter < arr.length && n > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        is_unsorted = true
+      end
+    }  
+    counter += 1
+  end
+  arr
 end
 
 puts bubble_sort([3,-1,9,2])
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,20 +1,20 @@
 def bubble_sort_by(arr)
-    is_sorted = false
-    counter = 1
-    while !is_sorted
-        is_sorted = true
-        arr.each_with_index { |n, i|
-            if i + counter < arr.length
-                result = yield(n, arr[i+1])
-                if result > 0
-                    arr[i], arr[i+1] = arr[i+1], arr[i]
-                    is_sorted = false
-                end                
-            end
-        }  
-        counter += 1
-    end
-    puts arr
+  is_unsorted = true
+  counter = 1
+  while is_unsorted
+    is_unsorted = false
+    arr.each_with_index { |n, i|
+      if i + counter < arr.length
+        result = yield(n, arr[i+1])
+        if result > 0
+          arr[i], arr[i+1] = arr[i+1], arr[i]
+          is_unsorted = true
+        end                
+      end
+    }  
+    counter += 1
+  end
+  puts arr
 end
 
 bubble_sort_by(["hi", "ho","hello","hey"]) do |left,right|
