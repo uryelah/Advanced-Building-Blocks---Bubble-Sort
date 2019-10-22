7d14387b87312d43c2be4d48c18ff6521ab3284e
sarah <sarachamorro@gmail.com>
2019-10-22 14:56:52 -0300

    STICKLER CORRECTIONS
    add space after commas
    add spaces around plus signs
    use next to scape loop

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -6,12 +6,11 @@ def bubble_sort_by(arr)
   while is_unsorted
     is_unsorted = false
     arr.each_with_index do |n, i|
-      if i + counter < arr.length
-        result = yield(n, arr[i+1])
-        if result > 0
-          arr[i], arr[i+1] = arr[i+1], arr[i]
-          is_unsorted = true
-        end
+      next if i + counter >= arr.length 
+      result = yield(n, arr[i + 1])
+      if result.positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        is_unsorted = true
       end
     end
     counter += 1
@@ -19,6 +18,6 @@ def bubble_sort_by(arr)
   puts arr
 end
 
-bubble_sort_by(["hi", "ho", "hello", "hey"]) do |left,right|
-left.length - right.length
+bubble_sort_by %W('hi', 'ho', 'hello', 'hey') do |left, right|
+  left.length - right.length
 end
