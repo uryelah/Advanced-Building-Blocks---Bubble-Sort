d63adf7008982343fdabc614a45526eee9c8b7a1
sarah <sarachamorro@gmail.com>
2019-10-22 14:49:29 -0300

    STICKLER CORRECTIONS

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 def bubble_sort_by(arr)
   is_unsorted = true
   counter = 1
   while is_unsorted
     is_unsorted = false
-    arr.each_with_index { |n, i|
+    arr.each_with_index do |n, i|
       if i + counter < arr.length
         result = yield(n, arr[i+1])
         if result > 0
           arr[i], arr[i+1] = arr[i+1], arr[i]
           is_unsorted = true
-        end                
+        end
       end
-    }  
+    end
     counter += 1
   end
   puts arr
 end
 
-bubble_sort_by(["hi", "ho","hello","hey"]) do |left,right|
+bubble_sort_by(["hi", "ho", "hello", "hey"]) do |left,right|
 left.length - right.length
 end
